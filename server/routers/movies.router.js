a56aa2b5486f9c10e43fbb9c7eddbb16d5a7b7fb
Matt Halvorson <halvorson34.mh@gmail.com>
2020-06-09 00:34:13 -0500

    add put route for movies router

--- server/routers/movies.router.js
+++ server/routers/movies.router.js
@@ -34,6 +34,22 @@ router.get("/details/:id", (req, res) => {
     });
 });
 
-router.put("/", (req, res) => {});
+router.put("/edit/:id", (req, res) => {
+  const queryText = `UPDATE "movies"
+  SET "title" = $1, "description" = $2
+  WHERE "id" = $3;`;
+  const movieId = req.params.id;
+  const newMovieData = req.body;
+
+  pool
+    .query(queryText, [newMovieData.title, newMovieData.description, movieId])
+    .then((responseDb) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.warn(err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
