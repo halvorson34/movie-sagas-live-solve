1d3d4828d5f19c768d503ca177cd9e1c708b6e37
Matt Halvorson <halvorson34.mh@gmail.com>
2020-06-08 22:38:25 -0500

    set up moviesRouter in server.js and routers folder

---
+++ server/routers/movies.router.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const pool = require("../modules/pool");
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+};
+
+router.get('/', (req, res) => {
+    
+};
+
+router.put('/', (req, res) => {
+    
+};
+
+module.export = router;
