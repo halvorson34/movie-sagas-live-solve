6fc308d86195406aa54209bf98ba484d60848e62
Matt Halvorson <halvorson34.mh@gmail.com>
2020-06-09 10:31:40 -0500

    add routes for HomePage and EditPage and set up HomePage and EditPage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,8 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import DetailsPage from "../pages/DetailsPage/DetailsPage";
+import HomePage from "../pages/HomePage/HomePage";
+import EditPage from "../pages/EditPage/EditPage";
 
 class App extends Component {
   // Renders the entire app on the DOM
@@ -10,9 +12,9 @@ class App extends Component {
     return (
       <div className="App">
         <Router>
-          {/* <Route exact path="/" component={} /> */}
+          <Route exact path="/" component={HomePage} />
           <Route path="/details" component={DetailsPage} />
-          {/* <Route path = "/edit" component = {} /> */}
+          <Route path="/edit" component={EditPage} />
         </Router>
       </div>
     );
