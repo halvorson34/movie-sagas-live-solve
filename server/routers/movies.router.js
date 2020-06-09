cc44e8a229381fb76c39b957663dc594a3d56c49
Matt Halvorson <halvorson34.mh@gmail.com>
2020-06-08 22:59:00 -0500

    add get route to get all movie data

--- server/routers/movies.router.js
+++ server/routers/movies.router.js
@@ -4,11 +4,21 @@ const pool = require("../modules/pool");
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    // get all movie data
+    const queryText = `SELECT * FROM "movies" ORDER BY "title" ASC;`;
 
-};
+    pool.query(queryText)
+    .then((responseDb) => {
+        res.send(responseDb.rows);
+    })
+    .catch((err) => {
+        console.warn(err);
+        res.sendStatus(500);
+    });
+});
 
 router.get('/', (req, res) => {
-    
+    // get single movie data
 };
 
 router.put('/', (req, res) => {
