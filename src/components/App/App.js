993f139f870f071bc596db85241519ecb5233fe1
Matt Halvorson <halvorson34.mh@gmail.com>
2020-06-09 10:26:00 -0500

    set up Routes for details page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+
+import DetailsPage from "../pages/DetailsPage/DetailsPage";
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <Router>
+          {/* <Route exact path="/" component={} /> */}
+          <Route path="/details" component={DetailsPage} />
+          {/* <Route path = "/edit" component = {} /> */}
+        </Router>
       </div>
     );
   }
